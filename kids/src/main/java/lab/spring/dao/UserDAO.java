64ab3afa7f6a20465df05aced1dba8a65d705101
MinjiKim <kimminji728@naver.com>
2019-11-03 15:54:39 +0900

    그래프 추가

---
+++ kids/src/main/java/lab/spring/dao/UserDAO.java
@@ -0,0 +1,33 @@
+package lab.spring.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import lab.spring.controller.DBConnection;
+import lab.spring.model.UserVO; 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+ 
+import javax.naming.NamingException;
+ 
+
+
+public interface UserDAO { 
+
+
+
+	public List<UserVO> getUserList() throws Exception; 
+	public UserVO getUserInfo(String userid) throws Exception; 
+	public int insertUser(UserVO userVO) throws Exception; 
+	public int updateUser(UserVO userVO) throws Exception; 
+	public int checkOverId(String user_id) throws Exception;
+	
+	
+	
+    
+	}
+
